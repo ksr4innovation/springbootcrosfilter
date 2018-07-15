16c1294eb8d73655ec11decc2f247d82792845b3
Kollu Sreenivasa Rao <kollu.srinu@gmail.com>
2018-07-15 20:39:37 +0530

    spring boot cros filter and registration

---
+++ WebConfigurer.java
@@ -0,0 +1,147 @@
+
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.MimeMappings;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+
+
+/**
+ * Configuration of web application with Servlet 3.0 APIs.
+ */
+@Configuration
+
+public class WebConfigurer implements ServletContextInitializer, EmbeddedServletContainerCustomizer {
+
+    private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
+
+    @Autowired
+    private Environment env;
+
+ 
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("Web application configuration, using profiles: {}", Arrays.toString(env.getActiveProfiles()));
+        EnumSet<DispatcherType> disps = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ASYNC);
+       
+        if (env.acceptsProfiles(Constants.SPRING_PROFILE_PRODUCTION)) {
+            initCachingHttpHeadersFilter(servletContext, disps);
+          //  initStaticResourcesProductionFilter(servletContext, disps);
+        }
+        initGzipFilter(servletContext, disps);
+        initCorsFilter(servletContext, disps);
+        log.info("Web application fully configured");
+    }
+
+    /**
+     * Set up Mime types.
+     */
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+        // IE issue, see https://github.com/jhipster/generator-jhipster/pull/711
+        mappings.add("html", "text/html;charset=utf-8");
+        // CloudFoundry issue, see https://github.com/cloudfoundry/gorouter/issues/64
+        mappings.add("json", "text/html;charset=utf-8");
+        container.setMimeMappings(mappings);
+    }
+
+    /**
+     * Initializes the GZip filter.
+     */
+    private void initGzipFilter(ServletContext servletContext, EnumSet<DispatcherType> disps) {
+        log.debug("Registering GZip Filter");
+        FilterRegistration.Dynamic compressingFilter = servletContext.addFilter("gzipFilter", new GZipServletFilter());
+        Map<String, String> parameters = new HashMap<>();
+        compressingFilter.setInitParameters(parameters);
+        compressingFilter.addMappingForUrlPatterns(disps, true, "*.css");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "*.json");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "*.html");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "*.js");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "/app/rest/*");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "/metrics/*");
+        compressingFilter.setAsyncSupported(true);
+    }
+
+    /**
+     * Initializes the static resources production Filter.
+     */
+    private void initStaticResourcesProductionFilter(ServletContext servletContext,
+                                                     EnumSet<DispatcherType> disps) {
+
+        log.debug("Registering static resources production Filter");
+        FilterRegistration.Dynamic staticResourcesProductionFilter =
+                servletContext.addFilter("staticResourcesProductionFilter",
+                        new StaticResourcesProductionFilter());
+
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/index.html");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/images/*");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/fonts/*");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/scripts/*");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/styles/*");
+        staticResourcesProductionFilter.setAsyncSupported(true);
+    }
+
+    /**
+     * Initializes the cachig HTTP Headers Filter.
+     */
+    private void initCachingHttpHeadersFilter(ServletContext servletContext,
+                                              EnumSet<DispatcherType> disps) {
+        log.debug("Registering Caching HTTP Headers Filter");
+        FilterRegistration.Dynamic cachingHttpHeadersFilter =
+                servletContext.addFilter("cachingHttpHeadersFilter",
+                        new CachingHttpHeadersFilter());
+
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/images/*");
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/fonts/*");
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/scripts/*");
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/styles/*");
+        cachingHttpHeadersFilter.setAsyncSupported(true);
+    }
+    
+    private void initCorsFilter(ServletContext servletContext, EnumSet<DispatcherType> disps) {
+        log.debug("Initializing CORS Filter");
+         FilterRegistration.Dynamic corsFilter =
+                    servletContext.addFilter("corsFilter",
+                            new CorsFilter());
+         corsFilter.addMappingForUrlPatterns(null, false, "/app/rest/*");
+         corsFilter.setAsyncSupported(true);
+       
+      } 
+
+
+    
+    
+//    @Bean
+//    public FilterRegistrationBean securityFilterChainRegistration( ) {
+//     DelegatingFilterProxy delegatingFilterProxy = new DelegatingFilterProxy();
+//     delegatingFilterProxy.setTargetBeanName("irisspnegoAuthenticationProcessingFilter");
+//     FilterRegistrationBean registrationBean = new FilterRegistrationBean(delegatingFilterProxy);
+//     registrationBean.addUrlPatterns("/app/rest/**");
+//     return registrationBean;
+//  }
+
+  
+}
